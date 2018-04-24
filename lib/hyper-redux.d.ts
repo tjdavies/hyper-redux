b4e7f35b8e6f9f025ef9f045e84032e937417e78
tjsdavies <tjsdavies@gmail.com>
2018-04-24 16:51:59 +0100

    new build

--- lib/hyper-redux.d.ts
+++ lib/hyper-redux.d.ts
@@ -4,6 +4,9 @@ export declare type ReducerFn<T, U> = (state: T, actions: U) => T;
 export declare type Actions<T> = {
     [P in keyof T]: T[P];
 };
+export declare function makeContextConsumer<T, U>(): React.Consumer<{
+    state: T;
+    actions: U;
+}>;
 export declare type View<T, U> = (state: T, actions: U) => JSX.Element;
-export declare const AppConsumer: React.ComponentType<React.ConsumerProps<{} | undefined>>;
 export declare function app<T, U>(initialState: T, reducer: U, view: View<T, U>, container: HTMLElement): any;
